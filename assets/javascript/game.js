fa9fe3ca9b9911e3e6bd66a93551c7cb24da9310
Kevin Martzolf <kmart914@gmail.com>
2017-07-12 15:44:08 -0700

    All 4 buttons add to total. Need to fix If statement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,11 +11,73 @@ var purpleCrystal= 0
 // Random number to be generated upon game start. This will be the number the user tries to get to.
 var random = randomNumber(19,120)
 
+var redRandom = randomNumber(1,12)
+var blueRandom = randomNumber(1,12)
+var greenRandom = randomNumber(1,12)
+var purpleRandom = randomNumber(1,12)
+
 var reset = function(){
 
 }
 
 
 function randomNumber(min,max){
-  return MAth.floor(Math.random()*(max-min+1)+min);
+  return Math.floor(Math.random()*(max-min+1)+min);
 }
+
+
+// When the page loads everything in fis function will be ready to be executed.
+$(document).ready(function() {
+
+  $("#play").click(function(){
+    document.getElementById("goalNumber").innerHTML = random;
+});
+
+  $("#redCrystal").click(function(){
+    totalScore += redRandom;
+    document.getElementById("currentScore").innerHTML = totalScore;
+
+});
+
+$("#blueCrystal").click(function(){
+  totalScore += blueRandom;
+  document.getElementById("currentScore").innerHTML = totalScore;
+
+});
+
+$("#purpleCrystal").click(function(){
+  totalScore += purpleRandom;
+  document.getElementById("currentScore").innerHTML = totalScore;
+
+});
+
+$("#greenCrystal").click(function(){
+  totalScore += greenRandom;
+  document.getElementById("currentScore").innerHTML = totalScore;
+
+});
+
+if (totalScore === random) {
+    document.getElementById("wins").innerHTML = wins++;
+    reset();
+} else if (totalScore > random){
+    document.getElementById("losses").innerHTML = losses++;
+    reset();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
