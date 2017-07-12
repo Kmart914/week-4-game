f110c0faf794b5ee632ba5e4d8dfd6a7b19fce7d
Kevin Martzolf <kmart914@gmail.com>
2017-07-11 18:30:28 -0700

    declared variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,3 +7,15 @@ var redCrystal = 0
 var blueCrystal= 0
 var greenCrystal= 0
 var purpleCrystal= 0
+
+// Random number to be generated upon game start. This will be the number the user tries to get to.
+var random = randomNumber(19,120)
+
+var reset = function(){
+
+}
+
+
+function randomNumber(min,max){
+  return MAth.floor(Math.random()*(max-min+1)+min);
+}
