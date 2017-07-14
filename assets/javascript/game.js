52a82c3334851e57dd80674f704182e8f6c86305
Kevin Martzolf <kmart914@gmail.com>
2017-07-13 18:25:34 -0700

    all functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,89 @@
+// When the page loads everything in fis function will be ready to be executed.
+$(document).ready(function() {
+
 //Variable declarations
 var wins = 0
 var losses = 0
-var goalNumber = 0
 var totalScore = 0
 var redCrystal = 0
 var blueCrystal= 0
 var greenCrystal= 0
 var purpleCrystal= 0
 
-// Random number to be generated upon game start. This will be the number the user tries to get to.
-var random = randomNumber(19,120)
 
-var redRandom = randomNumber(1,12)
-var blueRandom = randomNumber(1,12)
-var greenRandom = randomNumber(1,12)
-var purpleRandom = randomNumber(1,12)
+  //Reset after conditions are met
+  function startGame(){
+    goalNumber = randomNumber(19,120);
+    redRandom = randomNumber(1,12)
+    blueRandom = randomNumber(1,12)
+    greenRandom = randomNumber(1,12)
+    purpleRandom = randomNumber(1,12)
+    random = randomNumber(19,120)
 
-var reset = function(){
-
+    console.log("Goal" + goalNumber);
+    console.log("startGAme total score: ", totalScore);
+    $("#goalNumber").html(goalNumber);
 }
-
-
+//Generate random number for start and crystals
 function randomNumber(min,max){
   return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 
-// When the page loads everything in fis function will be ready to be executed.
-$(document).ready(function() {
+  //Loads goal number when page loads
+  $("#goalNumber").html(goalNumber);
 
-  $("#play").click(function(){
-    document.getElementById("goalNumber").innerHTML = random;
-});
-
+  //Red crystal button -- Attaches random number at start and reset.
   $("#redCrystal").click(function(){
     totalScore += redRandom;
-    document.getElementById("currentScore").innerHTML = totalScore;
+    $("#currentScore").html(totalScore);
+    gameCheck();
 
 });
 
 $("#blueCrystal").click(function(){
   totalScore += blueRandom;
-  document.getElementById("currentScore").innerHTML = totalScore;
+  $("#currentScore").html(totalScore);
+  gameCheck();
 
 });
 
 $("#purpleCrystal").click(function(){
   totalScore += purpleRandom;
-  document.getElementById("currentScore").innerHTML = totalScore;
+  $("#currentScore").html(totalScore);
+  gameCheck();
 
 });
 
 $("#greenCrystal").click(function(){
   totalScore += greenRandom;
-  document.getElementById("currentScore").innerHTML = totalScore;
-
+  $("#currentScore").html(totalScore);
+  gameCheck();
 });
 
-if (totalScore === random) {
-    document.getElementById("wins").innerHTML = wins++;
-    reset();
-} else if (totalScore > random){
-    document.getElementById("losses").innerHTML = losses++;
-    reset();
-  }
+function gameCheck(){
+
+  if (totalScore === goalNumber) {
+      wins++;
+      totalScore= 0;
+      $("#winCount").html(wins);
+      $("#currentScore").html(totalScore);
+      startGame();
+      alert("You win!")
+
+  } else if (totalScore > goalNumber){
+      losses++;
+      totalScore= 0;
+      $("#lossCount").html(losses);
+      $("#currentScore").html(totalScore);
+      startGame();
+      alert("You Lost! Try again!");
 
 
+  console.log(totalScore);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
+//Game start
+startGame();
 });
